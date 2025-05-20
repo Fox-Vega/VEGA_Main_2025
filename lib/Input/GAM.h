043b2ac6b99228467778be0f13c01209617fe712
Fox <Fox.Vega.Soft@gmail.com>
2025-05-20 12:30:35 +0900

    Update GAM.h

--- lib/Input/GAM.h
+++ lib/Input/GAM.h
@@ -17,45 +17,37 @@ class GAM { //いろいろエラーが出てたから弄った結果、GAMクラ
 
     private:
         //調整用
-        const float accel_noise = 0.12f; //動いていないと判断した際に使用するフィルタ（加速度センサーのノイズ除去）
-        const float movement_border = 0.15f; //動作判定ボーダー　accel_noiseよりは高い値を使用してください
-        const float adaptive_noise = 0.02f; //動いていると判断した際に使用するフィルタ
-        const float accel_tweaker = 0.4f; //この値が大きければ大きいほど、小さい値が増幅される
-        const float filterCoefficient = 0.95; //速度の平滑度合い　1に近いほど強い
-        const float reset_border = 3; //0が何回続いたらリセットするか
-        const float accel_offset_x = 1.1;
-        const float accel_offset_y = 1.0; 
+        const float movement_border = 0.15f; //動作判定ボーダー　accel_noiseより大きい値を使用するのがおすすめ
+        const float adaptive_noise = 0.02f; //動作判断時に使用するフィルタ
+        const float accel_noise = 0.12f; //静止判断時に使用するフィルタ
+        const float accel_tweaker = 0.4f; //この値が大きければ大きいほど、小さい値が増幅される(低速時加速度増幅目的)
+        const float filterCoefficient = 0.95; //1に近いほど値の平滑度合いが強い
+        const float reset_border = 3; //加速度変化無しが何回続いたら速度をリセットするか
+        const float accel_offset_x = 1.1; //x軸出力の倍率
+        const float accel_offset_y = 1.0; //y軸出力の倍率
 
-        int azimuth;
-        // int world_x;
-        // int world_y;
-        int yawtweak;
-        int zero_count[2];
-        int ten_count; //値を10にするかを決めるやつ
         int j;
-        int PoMi[2] = {1, 1}; //1は1~、0は~-1、10は0
-        int first_PoMi[2] = {10, 10};
         int world_x;
         int world_y;
+        int azimuth;
+        int yawtweak;
+        int ten_count;
+        int PoMi[2] = {1, 1}; //値の正負を保存する変数　1は1~、0は~-1、10は0
+        int first_PoMi[2] = {10, 10}; //最初に検出された値の正負を保存する変数
         bool move[2] = {false, false};
-        bool collision;
-        bool moving;
         float a;
         float b;
         float a_dt;
         float b_dt;
+        float speed[2];
+        float difcord_x;
+        float difcord_y;
+        float old_cordtime;
+        float old_speed[2];
         float lowpassValue[2];
         float highpassValue[2];
         float old_accel_data[2];
         float oold_accel_data[2];
-        float speed[2];
-        float old_speed[2];
-        float difcord_x;
-        float difcord_y;
-        float yaw_rad;
-        float old_cordtime;
-        float accel_bias[3] = {0.0, 0.0, 0.0};
-
-        //自己位置推定用
-        float states[6] = {0.0, 0.0};
+        float states[2] = {0.0, 0.0};
+        float accel_bias[2] = {0.0, 0.0};
 };
