e927c8255b83c9f25af1f93d5c53bb25caee998e
Fox <Fox.Vega.Soft@gmail.com>
2025-05-20 14:05:59 +0900

    スタートアップを変更

--- lib/AIP/General.cpp
+++ lib/AIP/General.cpp
@@ -16,33 +16,35 @@ void General::setup() {
 
 int General::startup() {
     phase = 1;
+    mypixel.brightness(100);
     while(phase != 4){
-        ball.read();
-        mypixel.closest(ball.get_azimuth(), 255, 0, 0, 1);
         switch_pressed = myswitch.check_tact();
         toggle_stat = myswitch.check_toggle();
-        mypixel.brightness(100);
+        if (phase != 3) {
+            if (mode == 1) {
+                mypixel.multis(0, 15, 255, 0, 0);
+            } else if (mode == 2) {
+                mypixel.multis(0, 15, 0, 0, 255);
+            } else if (mode == 3) {
+                mypixel.multis(0, 15, 255, 0, 255);
+            }
+            mypixel.unis(startPIXELs[startcord], 0, 255, 0);
+        }
         switch (phase) {
             case 1:
                 if (switch_pressed == 1){
                     mode = 1;
                     phase = 2;
-                    mypixel.clear();
-                    mypixel.multis(1, 16, 255, 0, 0);
                     mybuzzer.start(200, 4);
                     break;
                 } else if (switch_pressed == 2) {
                     mode = 2;
                     phase = 2;
-                    mypixel.clear();
-                    mypixel.multis(1, 16, 0, 0, 255);
                     mybuzzer.start(200, 4);
                     break;
                 } else if (switch_pressed == 3) {
                     mode = 3;
                     phase = 2;
-                    mypixel.clear();
-                    mypixel.multis(1, 16, 255, 0, 255);
                     mybuzzer.start(200, 4);
                     break;
                 }
@@ -55,10 +57,10 @@ int General::startup() {
                 } else if (switch_pressed == 2) {
                     if (startcord < 4) {
                         startcord += 1;
+                    } else {
+                        startcord = 0;
                     }
-                    mypixel.clear();
                     gam.cord_custom(startcords_x[startcord], startcords_y[startcord]);
-                    mypixel.unis(startPIXELs[startcord], 255, 0, 0);
                     mybuzzer.start(300, 4);
                     delay(100);
                     mybuzzer.start(300, 4);
@@ -75,21 +77,19 @@ int General::startup() {
                     break;
                 } else if (switch_pressed == 2) {
                     gam.dir_reset();
-                    mybuzzer.start(200, 2);
-                    delay(100);
-                    mybuzzer.start(100, 4);
+                    gam.cord_custom(startcords_x[startcord], startcords_y[startcord]);
+                    mybuzzer.start(300, 1);
                     break;
                 } else if (switch_pressed == 3) {
-                    gam.cord_custom(startcords_x[startcord], startcords_y[startcord]);
-                    mybuzzer.start(300, 4);
-                    delay(100);
-                    mybuzzer.start(300, 4);
                     break;
                 } else if (toggle_stat == 1) {
                     Run = true;
+                    phase = 4;
                 } else {
-                    mybuzzer.start(300, 8);
+                    mybuzzer.start(400, 8);
                     delay(100);
+                    mybuzzer.start(400, 8);
+                    delay(500);
                 }
         }
     }
