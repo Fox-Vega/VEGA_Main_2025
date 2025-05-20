bc543438ffc99a60843e31e8485c36da12cb13b9
Fox <Fox.Vega.Soft@gmail.com>
2025-05-20 16:24:00 +0900

    Update General.h

--- lib/AIP/General.h
+++ lib/AIP/General.h
@@ -6,8 +6,8 @@ class General {
     public:
         void setup(); //初期設定
         int startup(); //設定
-        int check_run(); //タクトスイッチを確認し、実行していいかを確認
-        int check_mode(); //モードを確認
+        int get_run(); //タクトスイッチを確認し、実行していいかを確認
+        int get_mode(); //モードを確認
         void update(); //値を更新
 
     private:
