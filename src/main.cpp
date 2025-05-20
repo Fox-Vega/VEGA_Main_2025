a672b54ff06a6ddb17f38f649b5c7dcc8b995871
Fox <Fox.Vega.Soft@gmail.com>
2025-05-20 16:20:21 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,16 @@
 
 
 void setup() {
-  general.setup();
+    general.setup();
 }
 
 void loop() {
-  gam.get_cord();
-  if (mode == 1) {
-    attack.attack_();
-  } else if (mode == 2) {
-    //defence
-  } else {
-    //test
-  }
+    gam.get_cord();
+    if (mode == 1) {
+        attack.attack_();
+    } else if (mode == 2) {
+        //defence
+    } else {
+        //test
+    }
 }
