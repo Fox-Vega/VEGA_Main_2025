e45f1fb78f350796dde5e6816f3e3be9a943ba4b
Fox <Fox.Vega.Soft@gmail.com>
2025-05-30 10:16:17 +0900

    Update MyVector.h

--- lib/AIP/MyVector.h
+++ lib/AIP/MyVector.h
@@ -6,7 +6,7 @@ class MyVECTOR {
     public:
         void get_cord(int azimuth, int magnitude);//座標を計算する関数
         void get_plpocord(int po_x, int po_y); //player to positionの座標を計算する関数　コート基準座標を入力　実行前に座標更新
-        void get_tarcord(int tar_azimuth, int tar_magnitude); //targetのコート座標を計算する関数　実行前に座標更新
+        void get_tarcord(int old_tar_azimuth, int old_tar_magnitude, int tar_azimuth, int tar_magnitude); //targetのコート座標を計算する関数　実行前に座標更新
         void get_svec(int tar_azimuth, int tar_magnitude); //targetの速度ベクトルを計算する関数　コート基準座標を入力　実行前に座標更新
         int get_azimuth(int x, int y); //方位角を計算する関数
         int get_magnitude(int x, int y); //距離を計算する関数
@@ -19,7 +19,7 @@ class MyVECTOR {
         int get_tarsvec_x(); //targetのコート基準速度ベクトルのx座標を返す関数
         int get_tarsvec_y(); //targetのコート基準速度ベクトルのy座標を返す関数
         int get_tarsvecazimuth(); //targetのコート基準速度ベクトルの方位角を返す関数
-        int get_tarsvecmagnitude(); //targetのコート基準速度ベクトルの方位角を返す関数
+        int get_tarsvecmagnitude(); //targetのコート基準速度ベクトルの大きさを返す関数
 
     private:
         int x, y;
