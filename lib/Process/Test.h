907af55b2d505f74d902c2b2ed4b2048fdba104f
Fox <Fox.Vega.Soft@gmail.com>
2025-05-31 21:10:39 +0900

    Update Test.h

--- lib/Process/Test.h
+++ lib/Process/Test.h
@@ -12,4 +12,6 @@ class Test {
     private:
         int t_mode = 1;
         int lastbuzzer = 0;
+        const byte motor_PIN1[4] = {8, 7, 5, 3};
+        const byte motor_PIN2[4] = {9, 6, 4, 2};
 };
