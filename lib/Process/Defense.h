9ccec57acd9d5eb864fb123eb98726d4d2025a80
okome onigiri <kusashiod@yahoo.co.jp>
2025-08-21 18:43:49 +0900

    Update Defense.h

--- lib/Process/Defense.h
+++ lib/Process/Defense.h
@@ -84,66 +84,7 @@ class Defense {
 
 
 
-        // --- 調整用定数 ---
 
-        // //倍率
-        // const float line_rate = 1;           // ラインの倍率
-        // const float ball_rate = 1;         // ボールの倍率
-        // const float motor_rate = 1;          // モーターの倍率
-
-        // //距離
-        // const uint16_t ball_max_X = 300;        // ボールの最大x座標（処理用）
-        // const uint8_t catch_ball_X = 10;       // ボールが目の前にあると判定するライン
-
-        // const uint8_t p5_t = 5;                   // p5のトリガー
-        // const uint8_t p2_back_t=3;               //　p2の戻る距離トリガー
-
-        // //速度
-        // const uint8_t p2_back_s=100;               // p2の戻る速度
-
-        // //タイマー
-        // const uint16_t p3_move_t = 3200; // 垂直ラインの移動処理用タイマー
-
-        // // --- ボール関連変数 ---
-        // int ball_azimuth;                  // ボールの方位角
-        // int ball_absolute_angle;           // 機体角度を考慮したボールの絶対角度
-        // int ball_dist;                     // ボールの距離
-        // float ball_x;                      // ボールのx座標
-        // float ball_y;                      // ボールのy座標
-        // bool ball_detect;                  // ボールが検出されているかどうか
-        // uint8_t ball_power = 0;               // ボールのパワー
-        // int ball_go_ang = 0;              // ボールの進行方向
-
-        // // --- ライン関連変数 ---
-        // int line_azimuth;                  // ラインの方位角
-        // uint8_t line_dist;                     // ラインの距離
-        // float line_x;                      // ラインのx座標
-        // float line_y;                      // ラインのy座標
-        // bool line_detect;                  // ラインが検出されているかどうか
-        // uint8_t line_type;                   // ラインの種類
-        // int line_go_ang;                   // ラインの進行方向
-        // uint8_t line_fb;                       // ラインの前後
-        // uint8_t line_power;                    // ラインのパワー
-
-        // // --- その他処理用変数 ---
-        // bool usePixel = true;              // ピクセルを使用するかどうか
-        // bool useBall = true;                // ボールを使用するかどうか
-        // bool useMotor = true;               // モーターを使用するかどうか
-        // bool useTimer = false;               // タイマーを使用するかどうか
-        // int line_sector;
-        // int r_azimuth;                     // ロボットの方位角
-        // int go_ang;                        // 進行方向
-        // int move_x;                        // 動くx
-        // int move_y;                        // 動くy
-        // uint8_t move_power;                // 動くパワー
-        // int move_azimuth;                  // 動く方位角
-        // int lastdetect = 180;              // 最後の検出角度 デフォルト後ろ
-        // int gotVector = 0;                 // ベクトル取得フラグ　多重取得防止やけど使ってない
-        // Timer d_timer;                     //デバッグ用
-        // Timer l_s;                         //line_same
-        // Timer p3_timer;                    // 垂直ラインの移動処理用タイマー
-        // int line_same=999;
-        // int line_his;
 
         // // --- メソッド ---
         // void get_vector(void);             // ベクトルを取得する関数
@@ -414,6 +355,67 @@ class Defense {
 
 // // void Defense::debug1(void){//デバッグ用 
 // // }
+
+        // --- 調整用定数 ---
+
+        // //倍率
+        // const float line_rate = 1;           // ラインの倍率
+        // const float ball_rate = 1;         // ボールの倍率
+        // const float motor_rate = 1;          // モーターの倍率
+
+        // //距離
+        // const uint16_t ball_max_X = 300;        // ボールの最大x座標（処理用）
+        // const uint8_t catch_ball_X = 10;       // ボールが目の前にあると判定するライン
+
+        // const uint8_t p5_t = 5;                   // p5のトリガー
+        // const uint8_t p2_back_t=3;               //　p2の戻る距離トリガー
+
+        // //速度
+        // const uint8_t p2_back_s=100;               // p2の戻る速度
+
+        // //タイマー
+        // const uint16_t p3_move_t = 3200; // 垂直ラインの移動処理用タイマー
+
+        // // --- ボール関連変数 ---
+        // int ball_azimuth;                  // ボールの方位角
+        // int ball_absolute_angle;           // 機体角度を考慮したボールの絶対角度
+        // int ball_dist;                     // ボールの距離
+        // float ball_x;                      // ボールのx座標
+        // float ball_y;                      // ボールのy座標
+        // bool ball_detect;                  // ボールが検出されているかどうか
+        // uint8_t ball_power = 0;               // ボールのパワー
+        // int ball_go_ang = 0;              // ボールの進行方向
+
+        // // --- ライン関連変数 ---
+        // int line_azimuth;                  // ラインの方位角
+        // uint8_t line_dist;                     // ラインの距離
+        // float line_x;                      // ラインのx座標
+        // float line_y;                      // ラインのy座標
+        // bool line_detect;                  // ラインが検出されているかどうか
+        // uint8_t line_type;                   // ラインの種類
+        // int line_go_ang;                   // ラインの進行方向
+        // uint8_t line_fb;                       // ラインの前後
+        // uint8_t line_power;                    // ラインのパワー
+
+        // // --- その他処理用変数 ---
+        // bool usePixel = true;              // ピクセルを使用するかどうか
+        // bool useBall = true;                // ボールを使用するかどうか
+        // bool useMotor = true;               // モーターを使用するかどうか
+        // bool useTimer = false;               // タイマーを使用するかどうか
+        // int line_sector;
+        // int r_azimuth;                     // ロボットの方位角
+        // int go_ang;                        // 進行方向
+        // int move_x;                        // 動くx
+        // int move_y;                        // 動くy
+        // uint8_t move_power;                // 動くパワー
+        // int move_azimuth;                  // 動く方位角
+        // int lastdetect = 180;              // 最後の検出角度 デフォルト後ろ
+        // int gotVector = 0;                 // ベクトル取得フラグ　多重取得防止やけど使ってない
+        // Timer d_timer;                     //デバッグ用
+        // Timer l_s;                         //line_same
+        // Timer p3_timer;                    // 垂直ラインの移動処理用タイマー
+        // int line_same=999;
+        // int line_his;
 
 
 };
