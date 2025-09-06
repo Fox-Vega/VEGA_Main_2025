e896ae699b3446bedc0aabf42e1e5e6b1462903e
Fox <fox.vega.soft@gmail.com>
2025-09-06 14:47:06 +0900

    a

--- lib/Input/Ball.cpp
+++ lib/Input/Ball.cpp
@@ -11,84 +11,33 @@ void BALL::setup() {
 }
 
 void BALL::read() {
-    total_x = 0;
-    total_y = 0;
-    max_ballvalue = 0;
-    // ballvalues の初期化
+    //センサー値初期化
     for (int i = 0; i < NUMball; i++) {
         ballvalues[i] = 0;
     }
 
-
-    timer.reset();
-
-    // センサー値の取得
-    while (timer.read_micro() < (833 * 4)) {
-    // for (int i = 0; i < 100; i++) {
-        for (int i = 0; i < NUMball; i++) {
-            if (digitalRead(ballPINs[i]) == LOW) {
-                ballvalues[i]++;
-                if (ballvalues[i] > max_ballvalue) { //最大値の記録
-                    max_ballvalue = ballvalues[i];
-                    max_ballNUM = i;
-                }
-            }
+    //センサー値取得
+    max_ballvalue = 0;
+    for (int i = 0; i < NUMball; i++) {
+        ballvalues[i] = pulseIn(ballPINs[i], LOW);
+        if (ballvalues[i] > max_ballvalue) { //最大値の記録
+            max_ballvalue = ballvalues[i];
+            max_ballNUM = i;
         }
     }
 
-    // ballNUMstart の補正
-    int ballNUMstart = (max_ballNUM + 14) % 16;
-
     // 座標計算
+    total_x = 0;
+    total_y = 0;
+    int ballNUMstart = (max_ballNUM + 14) % NUMball; //ベクトル移動平均計算開始センサー番号
     for (int i = 0; i < 5; i++) {
         int ballNUM = (ballNUMstart + i) % NUMball;
-
-        myvector.get_cord(balldirs[ballNUM], ball.get_value(ballNUM));
+        myvector.get_cord(balldirs[ballNUM], ballvalues[ballNUM]);
         total_x += myvector.get_x();
         total_y += myvector.get_y();
     }
-    ball_azimuth = myvector.get_azimuth(-total_x, -total_y);
-    // if (value[max_ballNUM] != 0) {
-    //     mypixel.closest(ball_azimuth, 255, 50, 50, 1, 1);
-    // }
-
-    //記録更新
-    for (int i = filter_size - 1; i > 0; i--) {
-        history[i] = history[i - 1];
-    }
-    if (value[max_ballNUM] != 0) {
-        history[0] = value[max_ballNUM];
-    } else {
-        history[0] = 0;
-    }
-}
-
-int BALL::get_value(short ballNUM) {
-    byte sensorNUM = ballNUM;
-    if (ballNUM == 99) {
-        sensorNUM = max_ballNUM;
-    }
-    if (ballvalues[sensorNUM] < detection_border) {
-        ballvalues[sensorNUM] = 0;
-    } else {
-        value[sensorNUM] = ballvalues[sensorNUM] * ballvalue_offset;
-    }
-    return value[sensorNUM];
-}
-
-int BALL::get_magnitude() {
-    int sum = 0;
-    for (int i = 0; i < filter_size; i++) {
-        sum += history[i];
-    }
-
-    int magnitude = sum / filter_size;
-    magnitude = max_value - magnitude;
-    old_magnitude = magnitude; // 過去の値を更新
-
-    return magnitude;
 }
 
 int BALL::get_azimuth() {
-    return ball_azimuth;
+    return myvector.get_azimuth(-total_x, -total_y);
 }
--- lib/Input/Ball.h
+++ lib/Input/Ball.h
@@ -7,30 +7,16 @@ class BALL {
         void setup(); //初期設定
         void read(); //センサーデータ取得
         int get_value(short ballNUM); //ボール値を取得する関数　99を使うと最大値を取得
-        int get_magnitude(); //ボールの距離を取得する関数
         int get_azimuth(); //ボールの方向を取得する関数
 
     private:
-        //調整用
-        const int ballvalue_offset = 30;
-        const int detection_border = 3; //反応ボーダー
-        const int max_value = 30 * ballvalue_offset;
-
-        const int filter_size = 20;
-        int history[20] = {0}; //フィルターサイズを変更したら配列数も変更。
-
-
         int value[16];
         int total_x = 0;
         int total_y = 0;
-        int ballvalue;
-        int ball_azimuth = 0;
-        int old_magnitude = 0;
-        int ballvalues[16]; //ボールの値を格納する配列
-        int total_magnitude = 10;
+        unsigned long ballvalues[16]; //ボールの値を格納する配列
         byte ballNUMstart;
         byte max_ballNUM = 0;
-        byte max_ballvalue = 0;
+        unsigned long max_ballvalue = 0;
         const byte NUMball = 16;
         const int ballPINs[16] = {31, 29, 27, 25, 23, 19, 17, 15, 32, 34, 36, 38, 40, 42, 44, 46};
         const float balldirs[16] = {0, 22.5, 45, 67.5, 90, 112.5, 135, 157.5, 180, 202.5, 225, 247.5, 270, 292.5, 315, 337.5}; //ボールセンサの角度
