ca930cdc2965a0ce8c9a63f97ad03376dbb3367e
Fox <fox.vega.soft@gmail.com>
2025-09-09 16:05:33 +0900

    a

--- lib/Process/Attack.cpp
+++ lib/Process/Attack.cpp
@@ -7,13 +7,15 @@
 void Attack::attack_() {
     if (line.get_type() != 0) {
         mymotor.run(line.get_eazimuth(), 200, 0);
-    } else {
+    } else if (ball.get_stat() == 1) {
         float ball_azimuth = ball.get_azimuth();
         if (ball_azimuth > 180) ball_azimuth -= 360;
-        ball_azimuth *= 1.5;
+        ball_azimuth *= 1.6;
         if (ball_azimuth < 0) {
             ball_azimuth += 360;
         }
         mymotor.run(ball_azimuth, 200, 0);
+    } else {
+        mymotor.free();
     }
 }
