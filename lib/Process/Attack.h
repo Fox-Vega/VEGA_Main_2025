dc7d67ec80825b44dbdcceac4a87b74dac188b3a
Fox <fox.vega.soft@gmail.com>
2025-09-12 21:51:12 +0900

    a

--- lib/Process/Attack.h
+++ lib/Process/Attack.h
@@ -10,10 +10,14 @@ class Attack {
     private:
         const short wrap_speed = 200;
         const short avoid_speed = 200; //ライン回避
-        const int wrap[1][2] = {{0, 0}}; //最初は0,0必須
+        const float wrap[3][2] = {//最初は0,0必須　最低2セット　2つ目に進む方向
+            {0, 0},
+            {15, 15},
+            {180, 150}
+        };
 
         short ball_dir;
         short face;
         int movement_azimuth;
-        size_t wrap_size;
+        int wrap_size;
 };
