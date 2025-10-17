92e3c3c05273bb644161070c40378fb159c89fde
okome onigiri <kusashiod@yahoo.co.jp>
2025-10-17 09:12:52 +0900

    ああああああああああ

--- lib/Process/Defense.h
+++ lib/Process/Defense.h
@@ -14,13 +14,15 @@ public:
 private:
     // === 調整用定数 ===
     //#define BALL_FILTER_OFF   // ボールフィルタを無効にする場合
-    #define USE_DASH false
+    #define USE_DASH true
     static constexpr float dash_border = 5000.0;        // ダッシュ待ち時間
     static constexpr float dash_time = 1500.0;          // ダッシュ時間
-    static constexpr float ball_move_border = 7.0;      // ボール移動境界(±角度)
+    static constexpr float ball_move_border = 5.0;      // ボール移動境界(±角度)
     static constexpr float move_speed = 220.0;          // 移動スピード（旧: ball_power）
     static constexpr float move_border = 40.0;          // 移動最小値 判定に使う
     static constexpr float line_late = 1.0;         // ライン反応倍率
+    static constexpr float y_late = 0.9;         // ライン反応倍率
+    static constexpr float x_late = 1.0;
     static constexpr float ball_late = 1.0;         // ボール反応倍率
 
     // === 処理用変数 ===
